a0ae92685bd3417cc978a2f87b2b8c9f96529f15
Guantecillo <Guantecillo@users.noreply.github.com>
2018-11-07 14:15:56 -0300

    actualizacion server
    prueba de despegue

--- Pruebas2/Torre_client.py
+++ Pruebas2/Torre_client.py
@@ -6,8 +6,8 @@ import Torre_pb2_grpc
 
 def Despegue():
     with grpc.insecure_channel('localhost:50051') as channel: #cambiar por ip+puerto
-        stub = Aeropuerto_pb2_grpc.PDespegueStub(channel)
-        response = stub.enviar_despegue(Aeropuerto_pb2.Pista_Des(Pista_despegue = 1, altura_despegue = 5000))
+        stub = Torre_pb2_grpc.PDespegueStub(channel)
+        response = stub.enviar_despegue(Torre_pb2.Pista_Des(autorizacion_despegue = True, Pista_despegue = 1, altura_despegue = 5000, posicion_despegue = 3, anterior_despegue = "alguien"))
     print("La respuesta es "+ response.flagDes)
 
 
@@ -16,19 +16,22 @@ message Pista_Des{
   bool autorizacion_despegue = 1;
   int32 Pista_despegue = 2;
   int32 altura_despegue = 3;
+  int32 posicion_despegue = 4;
+  string anterior_despegue = 5;
 }
 """
 if __name__ == '__main__':
     print("Bienvenido al Vuelo")
     aerolinea = input("[Avion] Nombre de la Aerolinea y numero de avion: ")
-    aerolinea = aerolinea.strip.split(' ')
+    aerolinea = aerolinea.strip().split( )
+    print(aerolinea)
     nombre = aerolinea[1]
-    peso = input("[Avion - "+nombre+"]: Peso maximo de carga [kg]: ")
-    estanque = input("[Avion - "+nombre+"]: Capacidad de combustible [L]: ")
+    peso = int(input("[Avion - "+nombre+"]: Peso maximo de carga [kg]: "))
+    estanque = int(input("[Avion - "+nombre+"]: Capacidad de combustible [L]: "))
     torre = input("[Avion - "+nombre+"]: IP de torre de control inicial: ")
     partir = input("[Avion - "+nombre+"]: desea despegar [S/N]: ")
 
-    while partir == n or partir == N:
+    while partir == 'n' or partir == 'N':
         time.sleep(15)
         partir = input("[Avion - "+nombre+"]: desea despegar [S/N]: ")
 
