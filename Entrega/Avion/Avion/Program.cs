d0d34f8f553676005fe469ddca23e463ec61dc3f
diegomontecinos <diegomontecinos@users.noreply.github.com>
2018-11-07 14:16:30 -0300

    Avion c#

---
+++ Entrega/Avion/Avion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Grpc.Core;
+using Aeropuerto;
+
+
+namespace Avion
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:500051", ChannelCredentials.Insecure);
+
+            var cliente = new Despegue.DespegueClient(channel);
+            String user = "Avion X";
+
+            var reply = cliente.enviar_despegue(new Pista_Des { IdAvion = user });
+            Console.WriteLine("Autprizacion" + reply.AutorizacionDespegue);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("presione alguna wea");
+            Console.ReadKey();
+
+
+
+          
+        }
+    }
+}
