cbd280ba8e683cee70543688fbf994fda94bdb52
Guantecillo <Guantecillo@users.noreply.github.com>
2018-11-06 23:23:39 -0300

    Update Pruebas2/Torre_server.py
    no sé como definir la función del main para correr el servidor

--- Pruebas2/Torre_server.py
+++ Pruebas2/Torre_server.py
@@ -9,17 +9,26 @@ import Torre_pb2_grpc
 
 #def asignar_pista():
 
-#def asignar_altura():
 
 
 
-def despegar(flag,pos,anterior):
+
+def despegar(flag,pos,anterior,altura,pista):
     with grpc.insecure_channel('localhost:50051') as channel: #cambiar por ip+puerto
-        stub = Aeropuerto_pb2_grpc.PDespegueStub(channel)
+        stub = Torre_pb2_grpc.Pista_DesStub(channel)
         if flag == 0:
-            response = stub.enviar_despegue(Aeropuerto_pb2.Pista_Des(autorizacion_despegue = False, Pista_despegue = asignar_pista(), altura_despegue = asignar_altura(),posicion_despegue = pos,anterior_despegue = anterior))
+            response = stub.enviar_despegue(Torre_pb2.Pista_Des(autorizacion_despegue = False, Pista_despegue = pista, altura_despegue = altura, posicion_despegue = pos,anterior_despegue = anterior))
         else:
-            response = stub.enviar_despegue(Aeropuerto_pb2.Pista_Des(autorizacion_despegue = True, Pista_despegue = asignar_pista(), altura_despegue = asignar_altura(),posicion_despegue = pos,anterior_despegue = anterior))
+            response = stub.enviar_despegue(Torre_pb2.Pista_Des(autorizacion_despegue = True, Pista_despegue = pista, altura_despegue = altura, posicion_despegue = pos,anterior_despegue = anterior))
+
+def aterrizar(flag,pos,anterior,altura,pista):
+    with grpc.insecure_channel('localhost:50051') as channel: #cambiar por ip+puerto
+        stub = Torre_pb2_grpc.Pista_AtgueStub(channel)
+        if flag == 0:
+            response = stub.enviar_aterrizaje(Torre_pb2.Pista_At(autorizacion_aterrizaje = False, Pista_aterrizaje = pista, altura_aterrizaje = altura, posicion_aterrizaje = pos,anterior_aterrizaje = anterior))
+        else:
+            response = stub.enviar_aterrizaje(Torre_pb2.Pista_At(autorizacion_aterrizaje = True, Pista_aterrizaje = pista, altura_aterrizaje = altura, posicion_aterrizaje = pos,anterior_aterrizaje = anterior))
+
 
 
 class PDespegue(Torre_pb2_grpc.PDespegueServicer):
@@ -28,6 +37,7 @@ class PDespegue(Torre_pb2_grpc.PDespegueServicer):
         self.nombre = input("Por favor ingrese nombre del aeropuerto: ")
         self.IP = input("Por favor ingrese la dirección IP del aeropuerto "+nombre+": ")
         self.pistas = input("Por favor ingrese cantidad de pistas del aeropuerto "+nombre+": ")
+        self.altura = 4050 #altura en metros
         self.Pentrada = dict()
         self.Pentrada["Pista 0"] = "Pista 0"
         self.Psalidas = dict()
@@ -54,25 +64,28 @@ class PDespegue(Torre_pb2_grpc.PDespegueServicer):
             if len(self.Pentrada["Pista "+str(i+1)])<self.pistas:
                 #if() revisar si está en esa lista y sacarlo de ahúí
                 #sacarlo de la cola
-                return despegar(1,-1,"nadie")
+                self.altura +=50
+                return despegar(1,-1,"nadie",self.altura,self.Pentrada["Pista 0"])
                 #break
         #anterior = colaDesp[-1] ver bien como ver el anterior
         aux = self.Pentrada["Pista 0"]
 
         self.Pentrada[aux].append(self.nombre)#es el nombre del avion
         pos = len(self.Pentrada[aux])-1
-        return despegar(0,pos,anterior)
+        return despegar(0,pos,anterior,0,aux)
 
     def enviar_aterrizaje (self, request, context):
         for i in range(self.pistas):
             if len(self.Psalidas["Pista "+str(i+1)])<self.pistas:
                 #ver si está en esa cola y sacarlo
-                return aterrizar(1,(i+1),"nadie")
+                self.altura +=50
+                return aterrizar(1,(i+1),"nadie",self.altura +=50,self.Psalidas["Pista 0"])
 
+        self.altura +=50
         aux = self.Psalidas["Pista 0"]
         self.Psalidas[aux].append() #nombre del avion
         pos = len(self.Psalidas[aux])-1
-        return aterrizar(0,pos,anterior)
+        return aterrizar(0,pos,anterior,self.altura,aux)
 
 
 
