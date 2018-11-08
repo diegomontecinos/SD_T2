203636fd08ecccddf5e3d7256c0e8f02f4da441c
diegomontecinos <diegomontecinos@users.noreply.github.com>
2018-11-08 02:55:39 -0300

    ac

--- Entrega/Avion/Avion/Program.cs
+++ Entrega/Avion/Avion/Program.cs
@@ -27,9 +27,11 @@ namespace Avion
             string user = Console.ReadLine();
             Console.WriteLine("Ingrese aeropuerto de destino: ");
             String destino = Console.ReadLine();
-            Console.WriteLine("Pasando por el Gate");
-            Console.WriteLine("Todos los pasajeros a bordo y combustible cargado.");
 
+            Console.WriteLine("Pasando por el Gate");
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("Todos los pasajeros a bordo y combustible cargado.");
+            System.Threading.Thread.Sleep(2000);
             Console.WriteLine("Pidiendo indicaciones para despegar.");
 
             var reply = cliente.enviar_despegue(new Pista_Des {IdAvion = user, NameDestino = destino});
@@ -42,44 +44,56 @@ namespace Avion
             Console.WriteLine("Ip: " + reply.IpDestino);
             Console.WriteLine("Id: " + reply.IdAvion);
 
-            if (reply.AutorizacionDespegue = true)
-            {
-                Console.WriteLine("despegando de la pista {0},  volando a una altura de {},", reply.PistaDespegue, reply.AlturaDespegue);
-            }
+            System.Threading.Thread.Sleep(2000);
 
             Resp_Cons second_reply = null;
 
-            if (reply.AutorizacionDespegue == false)
+            if (reply.AutorizacionDespegue == true)
             {
-                Console.WriteLine("Lamenatamos informar que todas las pistas se encuentran ocupadas, salremos luego del avion {0}, hay {1} aviones antes nuestro.", second_reply.AnteriorDespegue, second_reply.PosicionDespegue);
+                Console.WriteLine("despegando de la pista {0},  volando a una altura de {1},", reply.PistaDespegue, reply.AlturaDespegue);
+            }
+
+            else
+            {
+                Console.WriteLine("Lamenatamos informar que todas las pistas se encuentran ocupadas, salremos luego del avion {0}, hay {1} aviones antes nuestro.", reply.AnteriorDespegue, reply.PosicionDespegue);
                 second_reply = cliente.confirmar_despegue(new Cons_Des { IdAvion = user, NameDestino = destino, PistaEspera = reply.PistaDespegue, PosicionEspera = reply.PosicionDespegue });
 
                 string Pista_espera = second_reply.PistaDespegue;
-                int Posicion_espera = second_reply.PosicionDespegue;
 
-                while (second_reply.AutorizacionDespegue == false)
+                if (second_reply.AutorizacionDespegue == true)
                 {
-                    
-                    second_reply = cliente.confirmar_despegue(new Cons_Des { IdAvion = user, NameDestino = destino, PistaEspera =Pista_espera, PosicionEspera = Posicion_espera });
+                    Console.WriteLine("despegando de la pista {0},  volando a una altura de {1},", second_reply.PistaDespegue, second_reply.AlturaDespegue);
+                }
 
-                    if ( second_reply.PosicionDespegue = false)
+                else
+                {
+
+                    while (second_reply.AutorizacionDespegue == false)
                     {
-                        Console.WriteLine("Aun hay {0} aviones esperando para despegar.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Procederemos a despegar.");
+                        second_reply = cliente.confirmar_despegue(new Cons_Des { IdAvion = user, NameDestino = destino, PistaEspera = Pista_espera});
+
+
+                        if (second_reply.AutorizacionDespegue == true)
+                        {
+                            Console.WriteLine("Aun hay {0} aviones esperando para despegar.", second_reply.PosicionDespegue);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Procederemos a despegar.");
+                        }
                     }
                 }
+
+                Console.WriteLine("Autorizacion: {0} ", second_reply.AutorizacionDespegue);
+                Console.WriteLine("Pista: " + second_reply.PistaDespegue);
+                Console.WriteLine("Altura: " + second_reply.AlturaDespegue);
+                Console.WriteLine("Pos: " + second_reply.PosicionDespegue);
+                Console.WriteLine("Ant: " + second_reply.AnteriorDespegue);
+                Console.WriteLine("Ip: " + second_reply.IpDestino);
+                Console.WriteLine("Id: " + second_reply.IdAvion);
             }
 
-            Console.WriteLine("Autorizacion: " + second_reply.AutorizacionDespegue);
-            Console.WriteLine("Pista: " + second_reply.PistaDespegue);
-            Console.WriteLine("Altura: " + second_reply.AlturaDespegue);
-            Console.WriteLine("Pos: " + second_reply.PosicionDespegue);
-            Console.WriteLine("Ant: " + second_reply.AnteriorDespegue);
-            Console.WriteLine("Ip: " + second_reply.IpDestino);
-            Console.WriteLine("Id: " + second_reply.IdAvion);
+
 
             channel.ShutdownAsync().Wait();
             Console.WriteLine("presione cualquier tecla para salir: ");
--- Entrega/Torre/Torre_server.py
+++ Entrega/Torre/Torre_server.py
@@ -73,15 +73,22 @@ class Aeropuerto(Torre_pb2_grpc.Serv_AeropuertoServicer):
             self.Altura_de_despegue = Altura
             Anterior = "Eres el primero"
             Posicion = 0
-            print("el avion "+request.Id_avion+"Esta despegando por la pista "+ Pista)
+            print("el avion "+request.Id_avion+" esta despegando por la pista "+ Pista)
 
-        return Torre_pb2.Resp_Des(Autorizacion_despegue = Respuesta, Pista_despegue = Pista, Altura_despegue = Altura, Posicion_despegue = Posicion ,Anterior_despegue = Anterior, Ip_destino= request.Name_destino, Id_avion = request.Id_avion)
+        return Torre_pb2.Resp_Des(Autorizacion_despegue = Respuesta, Pista_despegue = Pista, Altura_despegue = Altura, Posicion_despegue = Posicion ,Anterior_despegue = Anterior, Ip_destino = self.Destinos[request.Name_destino], Id_avion = request.Id_avion)
 
     def confirmar_despegue(self, request, context):
 
-        self.Pistas_despegue
+        Pista = request.Pista_espera
 
-        if len (self.Pistas_despegue[request.Pista_espera]) > 0:
+        if len (self.Pistas_despegue[Pista]) > 0:
+
+            Respuesta = False
+            Altura = 0
+            Posicion = self.Pistas_despegue[Pista].index(request.Id_avion) +1
+            Anterior = self.Pistas_despegue[Pista][self.Pistas_despegue[Pista].index(request.Id_avion) -1]
+
+
             print ("el avion "+request.Id_avion+"Sigue esperando en la Posicion "+request.Posicion_espera)
 
         else:
@@ -93,6 +100,7 @@ class Aeropuerto(Torre_pb2_grpc.Serv_AeropuertoServicer):
             self.Pistas_despegue[request.Pista_espera].pop(0)
             print("el avion "+request.Id_avion+"Esta despegando por la pista "+request.request.Pista_espera)
 
+        return Torre_pb2.Resp_Cons(Autorizacion_despegue = Respuesta, Pista_despegue = Pista, Altura_despegue = Altura, Posicion_despegue = Posicion, Anterior_despegue = Anterior, Ip_destino = self.Destinos[request.Name_destino], Id_avion = request.Id_avion)
     def enviar_info(self, request, context):
         return Torre_pb2.Resp_Pantalla( Salida1 = "la wea 1", A_Donde1 = "a la chucha", Pista_Salida1 = "pista 1", Hora_Salida1 = "hora del pico", Salida2 = "la wea 2", A_Donde2 = "a la chucha 2", Pista_Salida2 = "pista 2", Hora_Salida2 = "hora del pico 2", Llegda1 = "tarde 1", De_donde1 = "donde tu vieja 1", Pista_Llegada1 = "pista 3", Hora_Llegada1 = "queti 1", Llegda2 = "tarde 2", De_donde2 = "donde tu vieja 2", Pista_Llegada2 = "pista 4", Hora_Llegada2 = "queti 2")
 
