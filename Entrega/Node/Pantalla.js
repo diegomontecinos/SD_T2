7f6ceed66e2ed2819f8fad9f60e6588a06e1f1c8
Guantecillo <Guantecillo@users.noreply.github.com>
2018-11-08 02:23:14 -0300

    Readme + pantalla
    la pantalla sigue sin funcar :c

--- Entrega/Node/Pantalla.js
+++ Entrega/Node/Pantalla.js
@@ -14,45 +14,55 @@ var packageDefinition = protoLoader.loadSync(
     });
 
 var packagePantalla = grpc.loadPackageDefinition(packageDefinition).Aeropuerto;
-//llamda al codigo contructor
 
 function main(){
-  console.log("entra al main");
-  //console.log("Ingrese IP del aeropuerto, si la torre esta en el mismo equipo, por favor escriba: localhost");
+
+  console.log("Ingrese IP del aeropuerto, si la torre esta en el mismo equipo, por favor escriba: localhost");
   //pide la ip de la torre
   var ipTorreAux = process.openStdin();
 
-  ipTorreAux.addListener("data", function(d) {
-      console.log("Ingrese IP del aeropuerto: " +
-          d.toString().trim());
-  });
+  //stdin.addListener("data", function(d) {
+  //    console.log("Tu nombre es: " +
+  //        d.toString().trim());
+  //  });
+
   //junta ip+puerto
   var puerto = ":50051";
   ipPuerto = ipTorreAux+puerto;
+  //console.log(ipPuerto);
 
-  var pantalla1 = new packagePantalla.Serv_Aeropuerto(ipPuerto,
+  var pantalla1 = new packagePantalla.Serv_Aeropuerto('localhost:50051',//ipPuerto,
                             grpc.credentials.createInsecure());
 
-  //editar para que sea una id para cada pantalla o que sea la ip de la pantalla
-  //console.log("Ingrese ID de Pantalla");
+
+  //pide id para cada pantalla
+  console.log("Ingrese ID de Pantalla");
   var idPantalla = process.openStdin();
 
-  idPantalla.addListener("data", function(d) {
-      console.log("Ingrese ID de Pantalla: " +
-          d.toString().trim());
-  });
-
-
-
+  //pedimos y mostramos info
 
   pantalla1.enviar_info({Nombre_Pantalla: idPantalla},function(err,response){
-    console.log("Departures\nAvion   Destino     Pista   Hr");
-    console.log("a",response.Salida1,'\t',response.A_Donde1,'\t',response.Pista_Salida1,'\t',response.Hora_Salida1);
+    console.log("Departures\nAvion\tDestino\tPista\tHr");
+    console.log(response.Salida1,'\t',response.A_Donde1,'\t',response.Pista_Salida1,'\t',response.Hora_Salida1);
     console.log(response.Salida2,'\t',response.A_Donde2,'\t',response.Pista_Salida2,'\t',response.Hora_Salida2);
     console.log("Arrivals\nAvion   Destino     Pista   Hr");
     console.log(response.Llegda1,'\t',response.De_donde1,'\t',response.Pista_Llegada1,'\t', response.Hora_Llegada1);
     console.log(response.Llegda2,'\t',response.De_donde2,'\t',response.Pista_Llegada2,'\t', response.Hora_Llegada2);
   });
+
+//  var cliente = new services.Serv_AeropuertoClient(ipPuerto,
+//                            grpc.credentials.createInsecure());
+//  var requuest = new messages.Id_Pantalla();
+//  request.setName(idPantalla);
+//  client.enviar_info(request,function(err, response){
+//    console.log("Departures\nAvion   Destino     Pista   Hr");
+//    console.log("a",response.Salida1,'\t',response.A_Donde1,'\t',response.Pista_Salida1,'\t',response.Hora_Salida1);
+//    console.log(response.Salida2,'\t',response.A_Donde2,'\t',response.Pista_Salida2,'\t',response.Hora_Salida2);
+//    console.log("Arrivals\nAvion   Destino     Pista   Hr");
+//    console.log(response.Llegda1,'\t',response.De_donde1,'\t',response.Pista_Llegada1,'\t', response.Hora_Llegada1);
+//    console.log(response.Llegda2,'\t',response.De_donde2,'\t',response.Pista_Llegada2,'\t', response.Hora_Llegada2);
+//  });
+
 
 }
 
---
+++ Entrega/README.txt
@@ -0,0 +1,12 @@
+para ejecutar el programa:
+
+
+Una vez que el programa se encuentre en ejecición:
+	la torre de control le pedirá que ingrese los siguientes datos: 
+		el nombre del aeropuerto, la ip del aeropuerto, la cantidad de pistas del aeropuero, 
+		para esta implementación se considera que la cantidad de pistas de aterrizaje es igual a las pistas de despegue.
+		Luego, se pedirá el ingreso de más destinos.
+	
+	los aviones le pedirá que ingrese los siguientes datos:
+		la IP de la torre de control inicial, la ID del avión, el cual correspode al número de avión,
+		Luego, le pedirá si quiere iniciar el vuelo o no.
